ac532084dcd34ca4b798a6100722d2c09bac11b9
Fazlemalak Chimthanawala <fazlemalak.asif@gmail.com>
2023-03-09 00:24:16 -0500

    Update Quarterback Rating Calc.py

--- Quarterback Rating Calc.py
+++ Quarterback Rating Calc.py
@@ -9,12 +9,15 @@ class Quarterback:
         self.td = t
         self.int = i
         self.QBR = 0
+
     def calc_QBR (self):
         self.QBR = 100 * (((self.com/self.att * 100 - 30)/20) + ((self.yd/self.att - 3)/4)\
                          + ((self.td/self.att * 100)/5) + ((1/4) * (9.5 - (self.int/self.att) * 100))) / 6
         return self.QBR
+
     def print_QBR (self):
         print("The Quarterback Rating is {:.1f}".format(self.calc_QBR()))
+
 
 def att_comp (a, c):
     while c > a:
@@ -24,25 +27,30 @@ def att_comp (a, c):
     while a <= 0:
         print("The value of passes attempted is incorrect")
         a = float(input("Enter the number of passes attempted : "))
+
+
 def comp_td (c, t):
     while t > c:
         print("The number of touchdowns cannot be greater than the number of passes completed")
         c = float(input("Enter the number of passes completed : "))
         t = float(input("Enter the number of touchdowns thrown : "))
+
+
 def gross_yards(y, c):
     while y > 99 * c or y < -20 * c:
         print("The number of yards passing is incorrect.")
         y = float(input("Enter the number of gross yards passing : "))
 
+
 # main
 
 a = float(input("Enter the number of passes attempted : "))
 c = float(input("Enter the number of passes completed : "))
-att_comp(a,c)
+att_comp(a, c)
 y = float(input("Enter the number of gross yards passing : "))
-gross_yards(y,c)
+gross_yards(y, c)
 t = float(input("Enter the number of touchdowns thrown : "))
-comp_td(c,t)
+comp_td(c, t)
 i = float(input("Enter the number of interceptions thrown : "))
-my_obj = Quarterback(a,c,y,t,i)
+my_obj = Quarterback(a, c, y, t, i)
 my_obj.print_QBR()
